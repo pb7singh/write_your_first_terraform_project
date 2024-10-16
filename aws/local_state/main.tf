618a9b23407987f67ab6bb8931afaed38d64d104
Aman <pb7singh@gmail.com>
2024-10-16 23:35:54 +0000

    data block moved to top

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -9,17 +9,10 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+
 provider "aws" {
   region  = "us-east-1"
 }
-
-resource "aws_instance" "app_server" {
-  ami           = "aws_ami.latest"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Terraform_Demo"
-  }
 
   data "aws_ami" "latest" {
   owners           = ["099720109477"]
@@ -31,6 +24,16 @@ resource "aws_instance" "app_server" {
 
  
   }
+
+resource "aws_instance" "app_server" {
+  ami           = "aws_ami.latest"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
+
+
 }
   
 
